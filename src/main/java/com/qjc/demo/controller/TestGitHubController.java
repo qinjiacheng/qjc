db6f514b2f7dbd2defba8b0ef13d6877433b9a83
qinjiacheng_glb <1358012993_glb@qq.com>
2021-02-04 14:31:03 +0800

    第er次提交

---
+++ src/main/java/com/qjc/demo/controller/TestGitHubController.java
@@ -0,0 +1,10 @@
+package com.qjc.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestGitHubController {
+    public String ii(){
+        return "";
+    }
+}
